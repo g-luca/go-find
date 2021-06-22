af8de3405d3e626057ba5ddce5e6b7f200748f0e
Luca <luca.g@protonmail.ch>
2021-06-22 09:24:56 +0200

    minor Profile UI fixes

--- src/views/ViewProfile/ViewProfile.vue
+++ src/views/ViewProfile/ViewProfile.vue
@@ -168,7 +168,7 @@
       <span v-else>
         <div class="min-w-screen flex items-center md:p-20 overflow-hidden relative">
           <div class="flex-1 min-h-full min-w-full md:rounded-3xl bg-gray-50 dark:bg-gray-900 shadow-2xl p-0 md:pl-20 text-gray-800 relative md:flex items-center text-center md:text-left">
-            <div class="w-full md:w-1/2 pt-20 md:pt-0">
+            <div class="w-full md:w-1/2 pt-20 lg:pt-0">
               <div class=" text-gray-600 font-light p-1 md:p-0">
                 <h1 class="font-extrabold uppercase text-6xl lg:text-8xl text-brand md:mb-5">don't panic</h1>
                 <h2 class="text-2xl lg:text-4xl dark:text-white font-bold ">The user is not here</h2>
