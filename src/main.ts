34bcb35cd6b15a2c0960529b501c1c596159bfe8
Luca <luca.g@protonmail.ch>
2021-07-25 16:52:43 +0200

    added Apollo GraphQL

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import { createApp } from 'vue'
+import { createApp, h } from 'vue'
 import App from './App.vue'
 import store from '@/store/'
 import router from './router'
 import './ui/tailwind.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import { apolloProvider } from './gql/Apollo'
 
-const app = createApp(App);
+const app = createApp({
+    render: () => h(App),
+});
+
 app.use(store);
 app.use(router);
+app.use(apolloProvider);
 app.mount('#app');
