0af64342f85f3793b3313067fdc7583a10306e57
Luca <luca.g@protonmail.ch>
2021-07-25 16:54:11 +0200

    migrated user search to graphQL  - added multi-result support

---
+++ src/gql/ProfileSearchQuery.ts
@@ -0,0 +1,19 @@
+import { gql } from "@apollo/client/core";
+
+/**
+ * @param usernaame A username string that can be a dtag, nickname or an ApplicationLink username. Followed by %
+ */
+export const ProfileSearchQuery = gql`
+    query ProfileSearch($query: String!) {
+        profile(where: {_or: [{dtag: {_ilike: $query}}, {nickname: {_ilike: $query}}, {application_links: {username: {_ilike: $query}}}]}) {
+            nickname
+            dtag
+            profile_pic
+            application_links {
+            application
+            username
+            }
+        }
+    }
+
+`;
