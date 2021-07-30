c77d72b95f6d85f376403d2eab0a2645c1055a6e
Luca <luca.g@protonmail.ch>
2021-07-30 22:56:26 +0200

    header z-index fix

--- src/ui/components/AppHeader/AppHeader.vue
+++ src/ui/components/AppHeader/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <header class="w-full shadow-lg bg-white dark:bg-gray-900 items-center h-16 z-40">
-    <div class="relative z-20 flex flex-col justify-center h-full px-3 mx-auto flex-center">
+  <header class="w-full shadow-lg bg-white dark:bg-gray-900 items-center h-16 z-0">
+    <div class="relative z-0 flex flex-col justify-center h-full px-3 mx-auto flex-center">
       <div class="relative items-center pl-1 flex w-full lg:max-w-68 sm:pr-2 sm:ml-0">
         <!-- Logo -->
         <div class="relative p-1 flex items-center justify-start mr-4 sm:mr-0 sm:left-auto">
@@ -15,7 +15,7 @@
           </router-link>
         </div>
 
-        <div class="container relative z-50 flex h-full justify-end">
+        <div class="container relative z-0 flex h-full justify-end">
           <!-- <ToggleTheme class="ml-4 my-auto" /> -->
 
           <!-- Search bar -->
