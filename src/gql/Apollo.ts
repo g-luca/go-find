c14b83610a0326d10efb78bf991af83d5af14a27
Luca <luca.g@protonmail.ch>
2021-08-01 22:37:44 +0200

    added ws support

--- src/gql/Apollo.ts
+++ src/gql/Apollo.ts
@@ -1,11 +1,31 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { ApolloClient, createHttpLink, InMemoryCache, split } from '@apollo/client/core'
 import { createApolloProvider } from '@vue/apollo-option'
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from 'apollo-utilities'
 
 // HTTP connection to the API
 const httpLink = createHttpLink({
     // You should use an absolute URL here
-    uri: process.env.VUE_APP_GRAPH_ENDPOINT
+    uri: `https://${process.env.VUE_APP_GRAPH_ENDPOINT}`,
 })
+/* 
+const wsLink = new WebSocketLink({
+    uri: `wss://${process.env.VUE_APP_GRAPH_ENDPOINT}`,
+    options: {
+        reconnect: true,
+    },
+})
+
+const link = split(
+    // split based on operation type
+    ({ query }) => {
+        const definition = getMainDefinition(query)
+        return definition.kind === 'OperationDefinition' &&
+            definition.operation === 'subscription'
+    },
+    httpLink,
+    wsLink,
+) */
 
 // Cache implementation
 const cache = new InMemoryCache()
