1b6f566c6b2c069ef3281013437fb2631a7d296f
Luca <luca.g@protonmail.ch>
2021-10-15 09:54:33 +0200

    added ModalTransaction alert for balance < 0

--- src/ui/components/ModalTransaction/ModalTransaction.vue
+++ src/ui/components/ModalTransaction/ModalTransaction.vue
@@ -71,45 +71,50 @@
                 {{ tx.$tx.$fee.$amount[0].$denom }}
               </span>
             </div> -->
-                <div class="pt-4">
-                  <button
-                    v-if="(!$store.state.AuthModule._account.isUsingKeplr&&inputMPassword.length>0)||$store.state.AuthModule._account.isUsingKeplr"
-                    :disabled="$store.state.TransactionModule.transactionStatus===1"
-                    type="button"
-                    :class="{'bg-gray-500 hover:bg-gray-500': $store.state.TransactionModule.transactionStatus===1, 'bg-indigo-600 hover:bg-indigo-700': $store.state.TransactionModule.transactionStatus!==1}"
-                    class="py-2 px-4 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                    @click="sign()"
-                  >
-                    <span
-                      v-if="$store.state.TransactionModule.transactionStatus===1"
-                      class="text-center"
+                <span v-if="$store.state.AccountModule.account._balance>0">
+                  <div class="pt-4">
+                    <button
+                      v-if="(!$store.state.AuthModule._account.isUsingKeplr&&inputMPassword.length>0)||$store.state.AuthModule._account.isUsingKeplr"
+                      :disabled="$store.state.TransactionModule.transactionStatus===1"
+                      type="button"
+                      :class="{'bg-gray-500 hover:bg-gray-500': $store.state.TransactionModule.transactionStatus===1, 'bg-indigo-600 hover:bg-indigo-700': $store.state.TransactionModule.transactionStatus!==1}"
+                      class="py-2 px-4 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                      @click="sign()"
                     >
-                      <svg
-                        class="animate-spin mx-auto h-7 w-7 text-brand"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
+                      <span
+                        v-if="$store.state.TransactionModule.transactionStatus===1"
+                        class="text-center"
                       >
-                        <circle
-                          class="opacity-25"
-                          cx="12"
-                          cy="12"
-                          r="10"
-                          stroke="currentColor"
-                          stroke-width="4"
-                        />
-                        <path
-                          class="opacity-75"
-                          fill="currentColor"
-                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                        />
-                      </svg>
-                    </span>
-                    <span v-else>
-                      Confirm
-                    </span>
-                  </button>
-                </div>
+                        <svg
+                          class="animate-spin mx-auto h-7 w-7 text-brand"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <circle
+                            class="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            stroke-width="4"
+                          />
+                          <path
+                            class="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                          />
+                        </svg>
+                      </span>
+                      <span v-else>
+                        Confirm
+                      </span>
+                    </button>
+                  </div>
+                </span>
+                <span v-else>
+                  <h1 class="text-md"><i class="bi bi-exclamation-triangle text-red-500" /> Your actual balance cannot cover transaction fees.</h1>
+                </span>
                 <div
                   v-if="$store.state.TransactionModule.transactionStatus===-1"
                   class="pt-2"
