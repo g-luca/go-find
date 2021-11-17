b9de237568ba8ff088dd9cecaa6a3a044a48d2cb
Luca <luca.g@protonmail.ch>
2021-11-17 11:02:10 +0100

    disabled non Cosmos chain-links

--- src/core/types/SupportedChainLinks.ts
+++ src/core/types/SupportedChainLinks.ts
@@ -3,10 +3,10 @@ import Blockchain from "./Blockchain";
 export const supportedChainLinks: Blockchain[] = [
     new Blockchain("desmos", "Desmos", "dsm", "desmos-mainnet", "m/44'/852'/0'/0/0", "desmos"),
     new Blockchain("cosmos", "Cosmos", "atom", "cosmoshub-4", "m/44'/118'/0'/0/0", "cosmos"),
-    new Blockchain("bitcoin", "Bitcoin", "btc", "", "m/44'/0'/0'/0/0", "bc1"),
-    new Blockchain("ethereum", "Ethereum", "eth", "", "m/44'/60'/0'/0/0", "0x"),
-    new Blockchain("dogecoin", "Dogecoin", "doge", "", "m/44'/3'/0'/0/0", "D"),
-    new Blockchain("cardano", "Cardano", "ada", "", "m/44'/1815'/0'/0/0", "A"),
+    /*  new Blockchain("bitcoin", "Bitcoin", "btc", "", "m/44'/0'/0'/0/0", "bc1"),
+     new Blockchain("ethereum", "Ethereum", "eth", "", "m/44'/60'/0'/0/0", "0x"),
+     new Blockchain("dogecoin", "Dogecoin", "doge", "", "m/44'/3'/0'/0/0", "D"),
+     new Blockchain("cardano", "Cardano", "ada", "", "m/44'/1815'/0'/0/0", "A"), */
     new Blockchain("band", "Band Protocol", "band", "band-guanyu-mainnet", "m/44'/494'/0'/0/0", "band"),
     new Blockchain("osmosis", "Osmosis", "osmo", "osmosis-1", "m/44'/118'/0'/0/0", "osmo"),
     new Blockchain("akash", "Akash", "akt", "akashnet-2", "m/44'/118'/0'/0/0", "akash"),
