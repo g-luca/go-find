de407630f65eddfaaa8601128c4cb31df9af3406
Luca <luca.g@protonmail.ch>
2021-11-27 11:58:10 +0100

    ux: minor chainlink improvements

--- src/core/types/SupportedChainLinks.ts
+++ src/core/types/SupportedChainLinks.ts
@@ -1,18 +1,17 @@
 import Blockchain from "./Blockchain";
 
 export const supportedChainLinks: Blockchain[] = [
-    new Blockchain("desmos", "Desmos", "dsm", "desmos-mainnet", "m/44'/852'/0'/0/0", "desmos"),
     new Blockchain("cosmos", "Cosmos", "atom", "cosmoshub-4", "m/44'/118'/0'/0/0", "cosmos"),
     /*  new Blockchain("bitcoin", "Bitcoin", "btc", "", "m/44'/0'/0'/0/0", "bc1"),
      new Blockchain("ethereum", "Ethereum", "eth", "", "m/44'/60'/0'/0/0", "0x"),
      new Blockchain("dogecoin", "Dogecoin", "doge", "", "m/44'/3'/0'/0/0", "D"),
      new Blockchain("cardano", "Cardano", "ada", "", "m/44'/1815'/0'/0/0", "A"), */
     new Blockchain("terra", "Terra", "LUNA", "columbus-5", "m/44'/330'/0'/0/0", "terra"),
-    new Blockchain("crypto", "Crypto.org", "cro", "crypto-org-chain-mainnet-1", "m/44'/118'/0'/0/0", "cro"),
     new Blockchain("osmosis", "Osmosis", "osmo", "osmosis-1", "m/44'/118'/0'/0/0", "osmo"),
-    new Blockchain("band", "Band Protocol", "band", "band-guanyu-mainnet", "m/44'/494'/0'/0/0", "band"),
     new Blockchain("akash", "Akash", "akt", "akashnet-2", "m/44'/118'/0'/0/0", "akash"),
+    new Blockchain("band", "Band Protocol", "band", "band-guanyu-mainnet", "m/44'/494'/0'/0/0", "band"),
     new Blockchain("regen", "Regen", "regen", "regen-1", "m/44'/118'/0'/0/0", "regen"),
+    new Blockchain("crypto", "Crypto.org", "cro", "crypto-org-chain-mainnet-1", "m/44'/118'/0'/0/0", "cro"),
     new Blockchain("sentinel", "Sentinel", "dvpn", "sentinelhub-2", "m/44'/118'/0'/0/0", "sent"),
     new Blockchain("kava", "Kava", "kava", "kava-3", "m/44'/459'/0'/0/0", "kava"),
     new Blockchain("emoney", "e-Money", "ngm", "emoney-3", "m/44'/118'/0'/0/0", "emoney"),
@@ -24,4 +23,5 @@ export const supportedChainLinks: Blockchain[] = [
     new Blockchain("persistence", "Persistence", "xprt", "core-1", "m/44'/750'/0'/0/0", "persistence"),
     new Blockchain("ixo", "IXO", "ixo", "impacthub-3", "m/44'/118'/0'/0/0", "ixo"),
     new Blockchain("likecoin", "Likecoin", "LIKE", "likecoin-mainnet-2", "m/44'/118'/0'/0/0", "cosmos"),
+    new Blockchain("desmos", "Desmos", "dsm", "desmos-mainnet", "m/44'/852'/0'/0/0", "desmos"),
 ];
