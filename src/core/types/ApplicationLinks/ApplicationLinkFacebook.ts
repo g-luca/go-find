21460ff1a012ff82b45332c9d3a6ea5157f4615b
g-luca <luca.g@protonmail.ch>
2022-12-15 17:25:53 +0100

    fix: icons paths

--- src/core/types/ApplicationLinks/ApplicationLinkFacebook.ts
+++ src/core/types/ApplicationLinks/ApplicationLinkFacebook.ts
@@ -4,7 +4,7 @@ import ApplicationLink from "../ApplicationLink";
 export default class ApplicationLinkFacebook extends ApplicationLink {
     readonly name: string = 'facebook';
     readonly displayName: string = 'Facebook';
-    readonly logo: string = '/public/assets/brands/facebook/logo.svg';
+    readonly logo: string = '/assets/brands/facebook/logo.svg';
     readonly url: string = "https://www.facebook.com/";
 
     constructor(username: string, state = ApplicationLinkState.APPLICATION_LINK_STATE_INITIALIZED_UNSPECIFIED) {
